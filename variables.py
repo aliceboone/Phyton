1d931359775910c93f98352657f12f737e05d3c4
Alice Boone <alicegbone@gmail.com>
2022-10-19 11:17:50 -0700

    variables

---
+++ variables.py
@@ -0,0 +1,49 @@
+# Variabble is a container where we put things inside
+# Create a variable and assign a value
+
+x = 2
+y = 3
+print(x + 6)
+print(x + y)
+
+name = "Youtube"
+print(f"{name} + rocks")
+
+# print string caracters
+print(name[1])
+print(name[3:7]) #print character from 3 to 5 on name
+
+wallet = 42
+print(wallet)
+
+# We can reassign a value to a variable
+wallet = 32
+print(wallet)
+
+day = 14
+print(day)
+
+temp = -14 # integers can be positiv or negative
+weight = 135.7 # float can be negative or positive
+
+print(3 +6)
+print(day + 3)
+
+print(weight * 2)
+
+height = 5.06
+age = 40
+
+print(height)
+print(age)
+
+shirt = "blue"
+print(shirt)
+
+day = 14
+month = "october"
+temp = 65
+
+print(f"Today is {month}, {day} and its {temp} degrees outside")
+
+baby_is_asllep = True  #boolean values are True and False
